2ec0aa047507548e8137ccf172643508315aac37
Josef Romero Florida <josef.romeroflorida@gmail.com>
2023-09-13 21:28:12 -0500

    Message

---
+++ src/main/java/com/monitech/restapi/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.monitech.restapi.repository;
+
+import com.monitech.restapi.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+
+}
